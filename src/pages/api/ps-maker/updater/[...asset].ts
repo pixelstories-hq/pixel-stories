7d2e21083e2bc06dfe503cdee2974d606ab4f4ab
tdgao <mr.trumgao@gmail.com>
2026-06-09 20:25:40 -0700

    fix: electron updater route

---
+++ src/pages/api/ps-maker/updater/[...asset].ts
@@ -0,0 +1,8 @@
+import type { APIRoute } from "astro";
+import { handlePsMakerUpdaterRequest } from "../../../../lib/ps-maker-updater";
+
+export const GET: APIRoute = async (context) =>
+  handlePsMakerUpdaterRequest(context, context.params.asset);
+
+export const HEAD: APIRoute = async (context) =>
+  handlePsMakerUpdaterRequest(context, context.params.asset);
